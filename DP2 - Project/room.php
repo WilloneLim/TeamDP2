653986ae1e5ba9cccd5c3c55369fb4d83cb7eaed
WilloneLim <4320719@students.swinburne.edu.my>
2017-10-03 12:02:54 +0800

    Added products pages

---
+++ DP2 - Project/room.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Room Decor</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet"/>
+    <link href="css/main.css" rel="stylesheet"/>
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">Deallo House Craft</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="home"><a href="index.php">Home</a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Products<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                       <li><a href="clothing.php">Clothing and Accesories</a></li>
+                        <li><a href="jewelry.php">Jewelry</a></li>
+                        <li><a href="crafts.php">Craft Supplies</a></li>
+                        <li><a href="room.php">Room Decor</a></li>
+                        <li><a href="toys.php">Soft Toys</a></li>
+                        <li><a href="arts.php">Vintage arts</a></li>
+                        <li><a href="wedding.php">Wedding Accesories</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Forums</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign UP</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login </a></li>
+            </ul>
+        </div>
+    </nav>
+	
+	
+        <div class="container-fluid">
+			<div class="Clothing-Accesories"><!--features_items-->
+			   <h2 class="title text-center">Room Decoration</h2>
+				<div class="col-sm-3">
+					<div class="product-image-wrapper">
+						<div class="single-products">
+						 <div class="products">
+						  <a href="#"><img src="image/carpet.jpg" class="img-responsive" alt="product"/></a>
+						    <div class="middle">
+							  <a href="#"><div class="text">Buy</div></a>
+							</div>
+							<div class="grid_1">
+						     <a href="#" class="item"><span class="price">$52<i></i></span></a>
+						    </div>
+						     <a href="#" class="button">Add to cart</a>
+							 <a href="#" class="wishlist"><i class="fa fa-plus-square"></i>Add to wishlist</a>
+					     </div>
+				        </div>
+				    </div>
+				</div>
+				
+				
+				<div class="col-sm-3">
+					<div class="product-image-wrapper">
+						<div class="single-products">
+						 <div class="products">
+						  <a href="#" ><img src="image/curtains.jpg" class="img-responsive" alt="product"/></a>
+						  <div class="middle">
+							  <a href="#"><div class="text">Buy</div></a>
+							</div>
+						    <div class="grid_1">
+						     <a href="#" class="item"><span class="price">$52<i></i></span></a>
+						    </div>
+						     <a href="#" class="button"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+							 <a href="#" class="wishlist"><i class="fa fa-plus-square"></i>Add to wishlist</a>
+					     </div>
+				        </div>
+				    </div>
+				</div>
+				
+				<div class="col-sm-3">
+					<div class="product-image-wrapper">
+						<div class="single-products">
+						 <div class="products">
+						  <a href="#" ><img src="image/frames.jpg" class="img-responsive" alt="product"/></a>
+						  <div class="middle">
+							  <a href="#"><div class="text">Buy</div></a>
+							</div>
+						    <div class="grid_1">
+						     <a href="#" class="item"><span class="price">$52<i></i></span></a>
+						    </div>
+						     <a href="#" class="button"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+							 <a href="#" class="wishlist"><i class="fa fa-plus-square"></i>Add to wishlist</a>
+					     </div>
+				        </div>
+				    </div>
+				</div>
+				
+				<div class="col-sm-3">
+					<div class="product-image-wrapper">
+						<div class="single-products">
+						 <div class="products">
+						  <a href="#" ><img src="image/lamp.jpg" class="img-responsive" alt="product"/></a>
+						  <div class="middle">
+							  <a href="#"><div class="text">Buy</div></a>
+							</div>
+						    <div class="grid_1">
+						     <a href="#" class="item"><span class="price">$52<i></i></span></a>
+						    </div>
+						     <a href="#" class="button"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+					         <a href="#" class="wishlist"><i class="fa fa-plus-square"></i>Add to wishlist</a>
+						 </div>
+				        </div>
+				    </div>
+				</div>
+				
+				<div class="col-sm-3">
+					<div class="product-image-wrapper">
+						<div class="single-products">
+						 <div class="products">
+						  <a href="#" ><img src="image/mirror.jpg" class="img-responsive" alt="product"/></a>
+						  <div class="middle">
+							  <a href="#"><div class="text">Buy</div></a>
+							</div>
+						    <div class="grid_1">
+						     <a href="#" class="item"><span class="price">$52<i></i></span></a>
+						    </div>
+						     <a href="#" class="button"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+							 <a href="#" class="wishlist"><i class="fa fa-plus-square"></i>Add to wishlist</a>
+					     </div>
+				        </div>
+				    </div>
+				</div>
+				
+				<div class="col-sm-3">
+					<div class="product-image-wrapper">
+						<div class="single-products">
+						 <div class="products">
+						  <a href="#" ><img src="image/table.jpg" class="img-responsive" alt="product"/></a>
+						  <div class="middle">
+							  <a href="#"><div class="text">Buy</div></a>
+							</div>
+						    <div class="grid_1">
+						     <a href="#" class="item"><span class="price">$52<i></i></span></a>
+						    </div>
+						     <a href="#" class="button"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+							 <a href="#" class="wishlist"><i class="fa fa-plus-square"></i>Add to wishlist</a>
+					     </div>
+				        </div>
+				    </div>
+				</div>
+				
+				<div class="col-sm-3">
+					<div class="product-image-wrapper">
+						<div class="single-products">
+						 <div class="products">
+						  <a href="#" ><img src="image/room.jpg" class="img-responsive" alt="product"/></a>
+						  <div class="middle">
+							  <a href="#"><div class="text">Buy</div></a>
+							</div>
+						    <div class="grid_1">
+						     <a href="#" class="item"><span class="price">$52<i></i></span></a>
+						    </div>
+						     <a href="#" class="button"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+							 <a href="#" class="wishlist"><i class="fa fa-plus-square"></i>Add to wishlist</a>
+					     </div>
+				        </div>
+				    </div>
+				</div>
+				
+				<div class="col-sm-3">
+					<div class="product-image-wrapper">
+						<div class="single-products">
+						 <div class="products">
+						  <a href="#" ><img src="image/livingroom.jpg" class="img-responsive" alt="product"/></a>
+						  <div class="middle">
+							  <a href="#"><div class="text">Buy</div></a>
+							</div>
+						    <div class="grid_1">
+						     <a href="#" class="item"><span class="price">$52<i></i></span></a>
+						    </div>
+						     <a href="#" class="button"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+							 <a href="#" class="wishlist"><i class="fa fa-plus-square"></i>Add to wishlist</a>
+					     </div>
+				        </div>
+				    </div>
+				</div>
+				
+				
+			</div>
+		</div>
+
