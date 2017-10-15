646bc156045482b83cc4cee53bc2a83725eca4d7
Skye95 <100072290@students.swinburne.edu.my>
2017-10-15 16:37:00 +0800

    made some changes to home page

--- DP2 - Project/index.php
+++ DP2 - Project/index.php
@@ -25,85 +25,65 @@
     
 
 
- <div class="row"> 
-  <div class="col-md-12">   
-   <div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
+   
+ 
+ <div id="myCarousel" class="carousel slide row" data-ride="carousel">
+    <!-- Indicators -->
      <ol class="carousel-indicators">
-       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-       <li data-target="#myCarousel" data-slide-to="1"></li>
-       <li data-target="#myCarousel" data-slide-to="2"></li>
-       <li data-target="#myCarousel" data-slide-to="3"></li>
-       <li data-target="#myCarousel" data-slide-to="4"></li>
-       <li data-target="#myCarousel" data-slide-to="5"></li>
-       <li data-target="#myCarousel" data-slide-to="6"></li>     
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+	  <li data-target="#myCarousel" data-slide-to="3"></li>
+	  <li data-target="#myCarousel" data-slide-to="4"></li>
+	  <li data-target="#myCarousel" data-slide-to="5"></li>
+	  <li data-target="#myCarousel" data-slide-to="6"></li>
      </ol>
 
-  <div class="carousel-inner">
-    <div class="item active">
-      <img src="image/clothing-carousel.jpeg" alt="clothing" class="carouselImage">
-        <div class="carousel-caption">
-            <h3>Clothing and Accesories</h3>
+  <!-- Wrapper for slides -->
+     <div class="carousel-inner">
+        <div class="item active">
+         <img src="image/newphoto1.jpeg" alt="clthes" class="img-responsive">
         </div>
-    </div>
 
-    <div class="item">
-      <img src="image/jewelry-carousel.jpeg" alt="Jewelry" class="carouselImage">
-        <div class="carousel-caption">
-            <h3>Jewelries</h3>
+        <div class="item">
+         <img src="image/newphoto2.jpg" alt="Jewellery" class="img-responsive">
         </div>
-    </div>
 
-    <div class="item">
-      <img src="image/roomDecor-carousel.jpeg" alt="Room-Decor" class="carouselImage">
-        <div class="carousel-caption">
-            <h3>Room Decorations</h3>
+        <div class="item">
+         <img src="image/newphoto3.jpeg" alt="roomstuff" class="img-responsive">
         </div>
-    </div>
-      
-    <div class="item">
-      <img src="image/Soft_Toys.jpg" alt="soft toys" class="carouselImage">
-        <div class="carousel-caption">
-            <h3>Soft Toys</h3>
+	
+	    <div class="item">
+         <img src="image/newphoto4_2.jpeg" alt="toyssoft" class="img-responsive">
         </div>
-    </div>
-      
-    <div class="item">
-      <img src="image/craft_supplies.jpeg" alt="Craft Supplies" class="carouselImage">
-        <div class="carousel-caption">
-            <h3>Craft Supplies</h3>
+	
+	    <div class="item">
+         <img src="image/newphoto4.jpeg" alt="paint" class="img-responsive">
         </div>
-    </div>
-      
-    <div class="item">
-      <img src="image/Vintage_art.jpg" alt="vintage art" class="carouselImage">
-        <div class="carousel-caption">
-            <h3>Vintage Art</h3>
+	
+	    <div class="item">
+         <img src="image/newphoto5.jpeg" alt="arts" class="img-responsive">
         </div>
-    </div>
-      
-    <div class="item">
-      <img src="image/Wedding_accessories.jpeg" alt="wedding accessories" class="carouselImage">
-        <div class="carousel-caption">
-            <h3>Wedding Accessories</h3>
+	
+	    <div class="item">
+         <img src="image/newphoto6.jpeg" alt="marriage" class="img-responsive">
         </div>
-    </div>  
+     </div>
 
-
-  <!-- Left and right controls -->
-       
+     <!-- Left and right controls -->
       <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-        <span class="sr-only">Previous</span>
+          <span class="glyphicon glyphicon-chevron-left"></span>
+          <span class="sr-only">Previous</span>
       </a>
       <a class="right carousel-control" href="#myCarousel" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-        <span class="sr-only">Next</span>
+         <span class="glyphicon glyphicon-chevron-right"></span>
+         <span class="sr-only">Next</span>
       </a>
-     </div>
-    </div>        
-  </div>  
- </div>    
+  </div>
+    
+    
+
+    
     
     <div class="container-fluid">
       <div class="row">   
